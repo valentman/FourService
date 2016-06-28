ace9afe8dc94dcebb843d64cae2c8498324c1ef2
P Joe <Joe.Pen@joepen.local>
2016-06-28 11:17:43 +0800

    Initial Commit

---
+++ FourService/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FourService
+//
+//  Created by Joe.Pen on 6/28/16.
+//  Copyright © 2016 Joe.Pen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
